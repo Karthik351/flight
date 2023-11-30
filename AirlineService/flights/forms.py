948dec65f69026faffd32f029580aa986f0c726a
Karthik351 <152361790+Karthik351@users.noreply.github.com>
2023-11-30 00:02:28 -0500

    Delete AirlineService directory

--- AirlineService/flights/forms.py
+++
@@ -1,9 +0,0 @@
-# AirlineService/forms.py
-
-from django import forms
-from .models import Passenger  # Import your models
-
-class AddPassengerForm(forms.ModelForm):
-    class Meta:
-        model = Passenger  # Use the appropriate model
-        fields = ['Passenger_ID', 'First_Name', 'Last_Name', 'Email', 'Phone_Number', 'Age', 'Passport_Number']
